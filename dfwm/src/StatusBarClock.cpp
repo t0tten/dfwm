0269ae857cbac887f5eb3bd8d9a4e52b2a3c8db4
Rasmus Schenström <element_004@hotmail.com>
2018-10-31 21:30:18 +0100

    Change clock :P

--- dfwm/src/StatusBarClock.cpp
+++ dfwm/src/StatusBarClock.cpp
@@ -15,9 +15,8 @@ std::string StatusBarClock::getTime() {
         time_t t = time(NULL);
         struct tm tm = *localtime(&t);
 
-        snprintf(buf, sizeof(buf), "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900,
-                        tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min,
-                        tm.tm_sec);
+        snprintf(buf, sizeof(buf), "[%d/%02d/%02d %02d:%02d]", tm.tm_year + 1900,
+                        tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min);
 
         std::string now = buf;
 
